ff3854f73bbc32ec1e4a2e8c2e65e5803c770725
Aeopp <irmxj777@gmail.com>
2021-05-05 19:39:51 +0900

    메인이랑 병합하고  TestScene 수정

--- Client/Source/TestScene.cpp
+++ Client/Source/TestScene.cpp
@@ -32,7 +32,6 @@
 #include <iostream>
 #include <fstream>
 
-
 using namespace std;
 TestScene::TestScene()
 {
@@ -116,7 +115,7 @@ HRESULT TestScene::LoadScene()
 	//}
 
 
-	//LoadMap();
+	LoadMap();
 	AddGameObject<TempMap>();
 
 	//AddGameObject<Glint>();
@@ -131,16 +130,16 @@ HRESULT TestScene::LoadScene()
 
 	// 렌더러 씬 맵 특성에 맞춘 세팅
 	auto _Renderer = Renderer::GetInstance();
-	//_Renderer->LightLoad("..\\..\\Resource\\LightData\\Mission02.json");
-	_Renderer->LightLoad("..\\..\\Resource\\LightData\\Light.json");
-	//_Renderer->CurSkysphereTex = _Renderer->SkyTexMission02Sunset;
-	//_Renderer->ao = 0.0005;
-	//_Renderer->SkyIntencity = 0.005f;
-	//_Renderer->SkysphereScale = 0.078f;
-	//_Renderer->SkysphereRot = { 0.f,0.f,0.f };
-	//_Renderer->SkysphereLoc = { 0.f,-2.3f,0.f };
-	//_Renderer->SoftParticleDepthScale = 0.7f;
-	//_Renderer->SkyRotationSpeed = 1.5f;
+	_Renderer->LightLoad("..\\..\\Resource\\LightData\\Mission02.json");
+	// _Renderer->LightLoad("..\\..\\Resource\\LightData\\Light.json");
+	_Renderer->CurSkysphereTex = _Renderer->SkyTexMission02Sunset;
+	_Renderer->ao = 0.0005;
+	_Renderer->SkyIntencity = 0.005f;
+	_Renderer->SkysphereScale = 0.078f;
+	_Renderer->SkysphereRot = { 0.f,0.f,0.f };
+	_Renderer->SkysphereLoc = { 0.f,-2.3f,0.f };
+	_Renderer->SoftParticleDepthScale = 0.7f;
+	_Renderer->SkyRotationSpeed = 1.5f;
 
 	//// Stage2 안개
 	//if (auto pSmoke = AddGameObject<Smoke>().lock();
