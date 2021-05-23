d9a482412d72ef045de64f799e8c91485a6e92fe
Aeopp <irmxj777@gmail.com>
2021-05-23 22:47:41 +0900

    push

--- Client/Header/FinalReady.h
+++ Client/Header/FinalReady.h
@@ -1,44 +1,48 @@
-//#ifndef __FINALREADY_H_
-//#define __FINALREADY_H_
-//#include "GameObject.h"
-//#include "RenderInterface.h"
-//#include <optional>
-//
-//class FinalReady : public ENGINE::GameObject,
-//				   public ENGINE::RenderInterface
-//{
-//private:
-//	std::shared_ptr<ENGINE::StaticMesh> _StaticMesh{};
-//	std::shared_ptr<ENGINE::Texture> _Albm{};
-//private:
-//	explicit FinalReady() = default;
-//	virtual ~FinalReady() = default;
-//	// GameObject을(를) 통해 상속됨
-//	virtual void Free() override;
-//	virtual std::string GetName() override;
-//public:
-//	static FinalReady* Create();
-//public:
-//	virtual void    RenderReady()                          override;
-//public:
-//	void			RenderInit();
-//	virtual HRESULT Ready() override;
-//	virtual HRESULT Awake() override;
-//	virtual HRESULT Start() override;
-//	virtual UINT    Update(const float _fDeltaTime) override;
-//	virtual UINT    LateUpdate(const float _fDeltaTime) override;
-//	virtual void    Editor()override;
-//	virtual void	OnEnable() override;
-//	virtual void    OnDisable() override;
-//public:
-//	void PlayStart(const Vector3& Location);
-//	void PlayEnd();
-//	void PlayParticle();
-//public:
-//	float T = 0.0f;
-//	float PlayTime = 5.f;
-//
-//	void RenderDebug(const DrawInfo& _Info);
-//	void RenderAlphaBlendEffect(const DrawInfo& _Info);
-//};
-//#endif //
+#ifndef __FINALREADY_H_
+#define __FINALREADY_H_
+#include "GameObject.h"
+#include "RenderInterface.h"
+#include <optional>
+
+class FinalReady : public ENGINE::GameObject,
+				   public ENGINE::RenderInterface
+{
+private:
+	std::shared_ptr<ENGINE::StaticMesh> _StaticMesh{};
+	std::shared_ptr<ENGINE::Texture>    _Alpg{};
+private:
+	explicit FinalReady() = default;
+	virtual ~FinalReady() = default;
+	// GameObject을(를) 통해 상속됨
+	virtual void Free() override;
+	virtual std::string GetName() override;
+public:
+	static FinalReady* Create();
+public:
+	virtual void    RenderReady()                          override;
+public:
+	void			RenderInit();
+	virtual HRESULT Ready() override;
+	virtual HRESULT Awake() override;
+	virtual HRESULT Start() override;
+	virtual UINT    Update(const float _fDeltaTime) override;
+	virtual UINT    LateUpdate(const float _fDeltaTime) override;
+	virtual void    Editor()override;
+	virtual void	OnEnable() override;
+	virtual void    OnDisable() override;
+public:
+	void PlayStart(const Vector3& Location);
+	void PlayEnd();
+	void PlayParticle();
+public:
+	float T = 0.0f;
+	float LerpEndT = 2.f;
+	float PlayTime = 5.f;
+
+	void RenderDebug(const DrawInfo& _Info);
+	void RenderAlphaBlendEffect(const DrawInfo& _Info);
+
+	// 에디터 변수
+	float ColorIntencity = 0.01f;
+};
+#endif //
