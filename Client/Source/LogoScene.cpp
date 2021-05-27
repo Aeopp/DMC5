d4d8a52935e13e62b3ebf7448ac573bb69152598
Aeopp <irmxj777@gmail.com>
2021-05-28 08:46:38 +0900

    ......

--- Client/Source/LogoScene.cpp
+++ Client/Source/LogoScene.cpp
@@ -58,7 +58,7 @@ HRESULT LogoScene::Update(const float _fDeltaTime)
 	{
 		// 다음 씬 로드
 		//SceneManager::LoadScene(TitleScene::Create(), false);
-		SceneManager::LoadScene(TestScene::Create(), false);
+		SceneManager::LoadScene(Hotel_S01::Create(), false);
 		//SceneManager::LoadScene(Hotel_S01::Create(), false);
 		//SceneManager::LoadScene(Hotel_S02::Create(), false);
 		//SceneManager::LoadScene(Hotel_S03::Create(), false);
