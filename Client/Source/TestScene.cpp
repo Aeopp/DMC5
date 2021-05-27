4603059163f1a2861d7e195528f95170dd95e046
Aeopp <irmxj777@gmail.com>
2021-05-27 17:13:53 +0900

    씬 복구

--- Client/Source/TestScene.cpp
+++ Client/Source/TestScene.cpp
@@ -112,9 +112,9 @@ HRESULT TestScene::LoadScene()
 
 #pragma region Player & Camera
 
-	_Camera = AddGameObject<Camera>();
+	// _Camera = AddGameObject<Camera>();
 	
-	// _MainCamera = AddGameObject<MainCamera>();
+	 _MainCamera = AddGameObject<MainCamera>();
 	_Player     = AddGameObject<Nero>();
    
 #pragma endregion
@@ -251,11 +251,11 @@ HRESULT TestScene::Update(const float _fDeltaTime)
 {
 	Scene::Update(_fDeltaTime);
 
-	if (auto SpPlayer = _Player.lock();
+	/*if (auto SpPlayer = _Player.lock();
 		SpPlayer)
 	{
 		SpPlayer->GetComponent<Transform>().lock()->SetPosition(Vector3{0.f,0.12f ,0.f});
-	}
+	}*/
 	
 	CheckShopAvailable();
 
